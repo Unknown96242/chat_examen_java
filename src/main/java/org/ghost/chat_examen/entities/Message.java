3ac13b6ac805359c6f1af718456c73c9419488ca
MonsterStackys <stackysjalan@groupeisi.com>
2026-03-06 18:09:20 +0000

    Ajout des entités

--- src/main/java/org/ghost/chat_examen/entities/Message.java
+++ src/main/java/org/ghost/chat_examen/entities/Message.java
@@ -1,1 +1,41 @@
+package org.ghost.chat_examen.entities;
 
+import lombok.*;
+import org.ghost.chat_examen.enums.MessageStatut;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "messages")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private User receiver;
+
+    @Column(length = 1000, nullable = false)
+    private String contenu;
+
+    private LocalDateTime dateEnvoi;
+
+    @Enumerated(EnumType.STRING)
+    private MessageStatut statut;
+
+    @PrePersist
+    protected void onSend() {
+        this.dateEnvoi = LocalDateTime.now();
+        this.statut = MessageStatut.ENVOYE;
+    }
+}
--- src/main/java/org/ghost/chat_examen/entities/User.java
+++ src/main/java/org/ghost/chat_examen/entities/User.java
@@ -1,1 +1,41 @@
+package org.ghost.chat_examen.entities;
 
+import lombok.*;
+import org.ghost.chat_examen.enums.Role;
+import org.ghost.chat_examen.enums.Status;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    private LocalDateTime dateCreation;
+
+
+    @PrePersist
+    protected void onCreate() {
+        this.dateCreation = LocalDateTime.now();
+        this.status = Status.OFFLINE;
+    }
+}
